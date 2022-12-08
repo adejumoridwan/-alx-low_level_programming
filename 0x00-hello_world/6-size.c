2452d5c9d820e4547bece860e107c00fc0399a28
adejumoridwan <adejumo999@gmail.com>
2022-12-08 15:26:19 +0100

    alx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+/*
+ * File - 6-size.c
+ * Author - Adejumo Ridwan Suleiman
+ */
+#include <stdio.h>
+/**
+ * main - print the size of various types on the comp
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}

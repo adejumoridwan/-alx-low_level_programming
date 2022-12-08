1326dadd94bf97e569a24fdf735f356e17e59521
adejumoridwan <adejumo999@gmail.com>
2022-12-08 14:56:58 +0100

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/*
+ * File: 5-printf.c
+ * Author: Adejumo Ridwan Suleiman
+ */
+#include <stdio.h>
+/**
+ * main - print "with proper grammar, but the outcome is a piece of art"
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

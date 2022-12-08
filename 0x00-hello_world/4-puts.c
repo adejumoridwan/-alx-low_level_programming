3d486d5941e687ca5a2685023abb6f6b6de1d1fd
adejumoridwan <adejumo999@gmail.com>
2022-12-08 14:49:29 +0100

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
+/*
+ * File: 4-puts.c
+ * Author: Adejumo Ridwan Suleiman
+ */
 #include <stdio.h>
-
+/**
+ * main -  print the statement
+ * "Programming is like building a multilingual puzzle"
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-

b721cc3304bfe06f1cc294fef29b0bc36dfb2ea1
adejumoridwan <adejumo999@gmail.com>
2022-12-10 17:19:04 +0100

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File - 5-print_numbers.c
+ * Author: Adejumo Ridwan Suleiman
+ */
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+
+	printf("\n");
+
+	return (0);
+}

6ca8ed9aff4af9cc936d19f548b45c7fd0a0b5a6
adejumoridwan <adejumo999@gmail.com>
2022-12-10 15:31:32 +0100

    alx

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+/* 
+ * File: 0-positive_or_negative.c
+ * Author: Adejumo Ridwan Suleiman
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* 
+ * main - function to return a diffeent value of n every time the program is run
+ * return: Return 0 
+ * */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n",n);
+	}
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+	return (0);
+}

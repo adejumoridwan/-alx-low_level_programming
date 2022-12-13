53f45b7d56b4f32cc769b35069428240820cae1f
adejumoridwan <adejumo999@gmail.com>
2022-12-13 10:56:05 +0100

    alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/*
+ * File - 0-puchar.c
+ * Author - Adejumo Ridwan Suleiman
+ */
+#include <stdio.h>
+/**
+ *printWord - print the word "_putchar"
+ */
+void printWord(void)
+{
+	printf("_putchar\n");
+}
+/**
+ *main - print the word "_putchar"
+ *Return: always 0
+ */
+int main(void)
+{
+	printWord();
+
+	return (0);
+}

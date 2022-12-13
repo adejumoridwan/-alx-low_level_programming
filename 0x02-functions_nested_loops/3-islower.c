9d9fd9e3de7e91806cd501cb7ce6814caefc651a
adejumoridwan <adejumo999@gmail.com>
2022-12-13 14:59:27 +0100

    alx

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - check if a number is lowercase
- * @c: the integer value it receives
- * Return: Always 0
+ * @c: the integer value it receiveis
+ * Return: 1 if true. 0 if false
  */
 int _islower(int c)
 {
